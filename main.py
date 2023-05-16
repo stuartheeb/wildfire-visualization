1ec6f3ef6d54b7b6f62e5864b2a9d0ca46003cff
stheeb <stheeb@student.ethz.ch>
2023-05-16 20:42:43 +0200

    Transfer function editing for vorticity (todo: load vorticity colormap from colormap.py)

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import numpy as np
 import vtk
 from vtkmodules.vtkInteractionStyle import vtkInteractorStyleTrackballCamera
 
-from callbacks import vtkSliderCallback, vtkButtonCallback
+from callbacks import vtkSliderCallback, vtkButtonCallback, vtkTransferFunctionButtonCallback
 from render import renderVolume, renderStreamline, renderSurface
 import gui
 
+from vtkEasyTransfer import vtkEasyTransfer
+
 def main(datadir, num_frames):
-    print(str(num_frames) + " frames to render")
+    print("Loading " + str(datadir) + ", there are " + str(num_frames) + " frames to render")
     # ---------- load data --------------------
     dataset = np.load(os.path.join(datadir, 'preprocessed/output.1000.npz'))
 
@@ -56,12 +58,32 @@ def main(datadir, num_frames):
     renderWindowInteractor.SetInteractorStyle(vtkInteractorStyleTrackballCamera())
     renderWindowInteractor.SetRenderWindow(renderWindow)
 
+
+    # --------------- transfer function --------------------
+    easyTransfer = vtkEasyTransfer()
+    easyTransfer.SetColormapHeat()  # set initial color map
+    easyTransfer.SetColorRange(0.28494199737906456, 3.1813182236022746)  # set the value range that is mapped to color
+    easyTransfer.SetOpacityRange(0, 0.1525)  # set the value range that is mapped to opacity
+    easyTransfer.RefreshImage()
+
+    # assign transfer function to volume properties
+    volumeProperty = vtk.vtkVolumeProperty()
+    volumeProperty.SetColor(easyTransfer.GetColorTransferFunction())
+    volumeProperty.SetScalarOpacity(easyTransfer.GetOpacityTransferFunction())
+
+    # This transfer function is for vorticity
+    vortActor.SetProperty(volumeProperty)
+
+    transferRenderer = easyTransfer.GetRenderer()
+    transferRenderer.SetViewport([0.8, 0.2, 1, 0.8])
+
     # -------------- GUI -----------------------------
     streamlineButton = vtk.vtkButtonWidget()
     vorticityButton = vtk.vtkButtonWidget()
+    editTransferFunctionToggle = vtk.vtkButtonWidget()
     sliderWidget = vtk.vtkSliderWidget()
     gui.setup(renderer, 
-              {'vorticity': vorticityButton, 'streamline': streamlineButton}, 
+              {'transfer': editTransferFunctionToggle, 'vorticity': vorticityButton, 'streamline': streamlineButton}, 
               sliderWidget, num_frames, renderWindowInteractor)
 
     # ------------------- callbacks --------------------
@@ -69,9 +91,14 @@ def main(datadir, num_frames):
                                        watchDict={'windVelocityMag': windVelocityImage,
                                                   'windVelocity': windVelocityImage})
     streamlineButton.AddObserver("StateChangedEvent", streamlineCallback)
+
     vorticityCallback = vtkButtonCallback(renderer=renderer, actors=[vortActor], dataset=dataset,
                                           watchDict={'vorticity': vortImage})
     vorticityButton.AddObserver("StateChangedEvent", vorticityCallback)
+
+    transferCallback = vtkTransferFunctionButtonCallback(renderWindow=renderWindow, transferRenderer=transferRenderer, renderWindowInteractor=renderWindowInteractor, interactor=vtkInteractorStyleTrackballCamera(), transferInteractor=easyTransfer)
+    editTransferFunctionToggle.AddObserver("StateChangedEvent", transferCallback)
+
     callbackSlider = vtkSliderCallback(datadir, buttonCallbacks=[streamlineCallback, vorticityCallback], dataset=dataset,
                                        watchDict={'theta': thetaImage, 'grass': grassImage, 
                                                   'vapor': vaporImage, 'soil': soilSurface,
